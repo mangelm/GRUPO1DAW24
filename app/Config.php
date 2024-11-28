12270517f7545aeec438130599d7b2e65e785fde
manfredi-01 <152871049+manfredi-01@users.noreply.github.com>
2024-11-28 14:34:30 +0100

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -4,7 +4,7 @@ class Config
 {
 //Modificaciones realizadas en la configuración de la base de datos para acceder a la base de datos creada.
     static public $mvc_bd_hostname = "localhost";
-    static public $mvc_bd_nombre   = "alimentos12020";
+    static public $mvc_bd_nombre   = "alimentos12024";
     static public $mvc_bd_usuario  = "myantoniofa";
     static public $mvc_bd_clave    = "f78jn8j7";
     static public $mvc_vis_css     = "estilo.css";
