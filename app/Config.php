300139dd927fea6d0ad523c8d7640373a4b6514c
manfredi-01 <152871049+manfredi-01@users.noreply.github.com>
2024-11-28 14:19:20 +0100

    Modificacion
    Modificacion  archivo Config.php y revision de index.php

---
+++ app/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+class Config
+{
+//Modificaciones realizadas en la configuración de la base de datos para acceder a la base de datos creada.
+    static public $mvc_bd_hostname = "localhost";
+    static public $mvc_bd_nombre   = "alimentos12020";
+    static public $mvc_bd_usuario  = "myantoniofa";
+    static public $mvc_bd_clave    = "f78jn8j7";
+    static public $mvc_vis_css     = "estilo.css";
+}
+?>
